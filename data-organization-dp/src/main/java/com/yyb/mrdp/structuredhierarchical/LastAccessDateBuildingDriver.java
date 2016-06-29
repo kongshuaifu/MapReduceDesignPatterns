9e797ce6c5575a954a05450db309d03d475bb1d2
yiyubin <1049105063@qq.com>
2016-06-29 20:56:17 +0800

    data-origanization

---
+++ data-organization-dp/src/main/java/com/yyb/mrdp/structuredhierarchical/LastAccessDateBuildingDriver.java
@@ -0,0 +1,102 @@
+package com.yyb.mrdp.structuredhierarchical;
+
+import com.yyb.mrdp.utils.MRDPUtils;
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Map;
+
+
+/**
+ * @author Administrator
+ *         2016/6/29.
+ */
+public class LastAccessDateBuildingDriver {
+
+    public static void main(String[] args) throws Exception{
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "LastAccessDateBuildingDriver");
+        // Set custom partitioner and min last access date
+        job.setPartitionerClass(LastAccessDatePartitioner.class);
+        LastAccessDatePartitioner.setMinLastAccessDate(job, 2008);
+        // Last access dates span between 2008-2011, or 4 years
+        job.setNumReduceTasks(4);
+
+    }
+
+    public static class LastAccessDateMapper extends
+            Mapper<Object, Text, IntWritable, Text> {
+        // This object will format the creation date string into a Date object
+        private final static SimpleDateFormat frmt = new SimpleDateFormat(
+                "yyyy-MM-dd'T'HH:mm:ss.SSS");
+        private IntWritable outkey = new IntWritable();
+        protected void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+            Map<String, String> parsed = MRDPUtils.transformXmlToMap(value
+                .toString());
+// Grab the last access date
+            String strDate = parsed.get("LastAccessDate");
+// Parse the string into a Calendar object
+            Calendar cal = Calendar.getInstance();
+            try {
+                cal.setTime(frmt.parse(strDate));
+            }catch (Exception e){
+
+            }
+
+            outkey.set(cal.get(Calendar.YEAR));
+// Write out the year with the input value
+            context.write(outkey, value);
+        }
+    }
+
+
+    public static class LastAccessDatePartitioner extends
+            Partitioner<IntWritable, Text> implements Configurable {
+        private static final String MIN_LAST_ACCESS_DATE_YEAR =
+                "min.last.access.date.year";
+        private Configuration conf = null;
+        private int minLastAccessDateYear = 0;
+
+        public int getPartition(IntWritable key, Text value, int numPartitions) {
+            return key.get() - minLastAccessDateYear;
+        }
+
+        public Configuration getConf() {
+            return conf;
+        }
+
+        public void setConf(Configuration conf) {
+            this.conf = conf;
+            minLastAccessDateYear = conf.getInt(MIN_LAST_ACCESS_DATE_YEAR, 0);
+        }
+
+        public static void setMinLastAccessDate(Job job,
+                                                int minLastAccessDateYear) {
+            job.getConfiguration().setInt(MIN_LAST_ACCESS_DATE_YEAR,
+                    minLastAccessDateYear);
+        }
+    }
+
+
+    public static class ValueReducer extends
+            Reducer<IntWritable, Text, Text, NullWritable> {
+        protected void reduce(IntWritable key, Iterable<Text> values,
+                              Context context) throws IOException, InterruptedException {
+            for (Text t : values) {
+                context.write(t, NullWritable.get());
+            }
+        }
+    }
+}
---
+++ data-organization-dp/src/main/java/com/yyb/mrdp/structuredhierarchical/QuestionAnswerBuildingDriver.java
@@ -0,0 +1,170 @@
+package com.yyb.mrdp.structuredhierarchical;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Administrator
+ *         2016/6/29.
+ */
+public class QuestionAnswerBuildingDriver {
+
+    public static class PostCommentMapper extends
+            Mapper<Object, Text, Text, Text> {
+        private DocumentBuilderFactory dbf = DocumentBuilderFactory
+                .newInstance();
+        private Text outkey = new Text();
+        private Text outvalue = new Text();
+
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+// Parse the post/comment XML hierarchy into an Element
+            try {
+                Element post = getXmlElementFromString(value.toString());
+
+                int postType = Integer.parseInt(post.getAttribute("PostTypeId"));
+// If postType is 1, it is a question
+                if (postType == 1) {
+                    outkey.set(post.getAttribute("Id"));
+                    outvalue.set("Q" + value.toString());
+                } else {
+// Else, it is an answer
+                    outkey.set(post.getAttribute("ParentId"));
+                    outvalue.set("A" + value.toString());
+                }
+                context.write(outkey, outvalue);
+            } catch (SAXException e) {
+                e.printStackTrace();
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private Element getXmlElementFromString(String xml) throws IOException, SAXException, ParserConfigurationException {
+// same as previous example, “Mapper code” (page 80)
+            DocumentBuilder bldr = dbf.newDocumentBuilder();
+            return bldr.parse(new InputSource(new StringReader(xml)))
+                    .getDocumentElement();
+        }
+    }
+
+    public static class QuestionAnswerReducer extends
+            Reducer<Text, Text, Text, NullWritable> {
+        private ArrayList<String> answers = new ArrayList<String>();
+        private DocumentBuilderFactory dbf = DocumentBuilderFactory
+                .newInstance();
+        private String question = null;
+
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+// Reset variables
+            question = null;
+            answers.clear();
+// For each input value
+            for (Text t : values) {
+                // If this is the post record, store it, minus the flag
+                if (t.charAt(0) == 'Q') {
+                    question = t.toString().substring(1, t.toString().length())
+                            .trim();
+                } else {
+// Else, it is a comment record. Add it to the list, minus
+// the flag
+                    answers.add(t.toString()
+                            .substring(1, t.toString().length()).trim());
+                }
+            }
+            try {
+
+
+// If post is not null
+            if (question != null) {
+// nest the comments underneath the post element
+                String postWithCommentChildren = nestElements(question, answers);
+// write out the XML
+                context.write(new Text(postWithCommentChildren),
+                        NullWritable.get());
+            }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        private String nestElements(String post, List<String> comments) throws Exception {
+// Create the new document to build the XML
+            DocumentBuilder bldr = dbf.newDocumentBuilder();
+            Document doc = bldr.newDocument();
+// Copy parent node to document
+            Element postEl = getXmlElementFromString(post);
+            Element toAddPostEl = doc.createElement("post");
+// Copy the attributes of the original post element to the new one
+            copyAttributesToElement(postEl.getAttributes(), toAddPostEl);
+// For each comment, copy it to the "post" node
+            for (String commentXml : comments) {
+                Element commentEl = getXmlElementFromString(commentXml);
+                Element toAddCommentEl = doc.createElement("comments");
+// Copy the attributes of the original comment element to
+// the new one
+                copyAttributesToElement(commentEl.getAttributes(),
+                        toAddCommentEl);
+// Add the copied comment to the post element
+                toAddPostEl.appendChild(toAddCommentEl);
+            }
+// Add the post element to the document
+            doc.appendChild(toAddPostEl);
+// Transform the document into a String of XML and return
+            return transformDocumentToString(doc);
+        }
+
+        private void copyAttributesToElement(NamedNodeMap attributes,
+                                             Element element) {
+// For each attribute, copy it to the element
+            for (int i = 0; i < attributes.getLength(); ++i) {
+
+                Attr toCopy = (Attr) attributes.item(i);
+                element.setAttribute(toCopy.getName(), toCopy.getValue());
+            }
+        }
+
+        private Element getXmlElementFromString(String xml) throws IOException, SAXException, ParserConfigurationException {
+// same as previous example, “Mapper code” (page 80)
+            DocumentBuilder bldr = dbf.newDocumentBuilder();
+            return bldr.parse(new InputSource(new StringReader(xml)))
+                    .getDocumentElement();
+        }
+
+        private String transformDocumentToString(Document doc) throws Exception {
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION,
+                    "yes");
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(doc), new StreamResult(
+                    writer));
+// Replace all new line characters with an empty string to have
+// one record per line.
+            return writer.getBuffer().toString().replaceAll("\n|\r", "");
+        }
+    }
+}
